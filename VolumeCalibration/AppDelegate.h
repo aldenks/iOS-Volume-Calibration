632898733c1620ae4c693d6491b1d2397b841555
aldenks <aldenkeefesampson@gmail.com>
2013-04-07 21:36:58 -0400

    Initial Commit

---
+++ VolumeCalibration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VolumeCalibration
+//
+//  Created by alden on 4/7/13.
+//  Copyright (c) 2013 Alden Keefe Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VolumeCalibration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VolumeCalibration
+//
+//  Created by alden on 4/7/13.
+//  Copyright (c) 2013 Alden Keefe Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
