3c77b6e6bfa67ca71cb805406e892e174c68a5fd
aldenks <aldenkeefesampson@gmail.com>
2013-04-07 23:12:02 -0400

    first commit. all basic functionality working

--- VolumeCalibration/ViewController.h
+++ VolumeCalibration/ViewController.h
@@ -7,7 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVAudioPlayer.h>
 
+// toneFiles = @[@"8000Hz", @"9000Hz", @"10000Hz", @"11200Hz", @"12500Hz", @"14000Hz", @"16000Hz"];
 @interface ViewController : UIViewController
 
+@property NSArray *players;
+@property AVAudioPlayer *currentPlayer;
+
+@property (weak, nonatomic) IBOutlet UISwitch *playingSwitch;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *toneSelector;
+@property (weak, nonatomic) IBOutlet UITextField *volumeDisplay;
+@property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
+
+- (IBAction)playingSwitchValueChanged:(id)sender;
+- (IBAction)toneSelectorValueChanged:(id)sender;
+- (IBAction)volumeChanged:(id)sender;
 @end
--- VolumeCalibration/ViewController.m
+++ VolumeCalibration/ViewController.m
@@ -16,8 +16,24 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+
+  NSURL *soundURL;
+  soundURL = [[NSBundle mainBundle] URLForResource:@"8000Hz"  withExtension:@"mp3"];
+  AVAudioPlayer *pl8000  = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
+  pl8000.numberOfLoops  = -1;
+  soundURL = [[NSBundle mainBundle] URLForResource:@"10000Hz" withExtension:@"mp3"];
+  AVAudioPlayer *pl10000 = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
+  pl10000.numberOfLoops = -1;
+  soundURL = [[NSBundle mainBundle] URLForResource:@"11200Hz" withExtension:@"mp3"];
+  AVAudioPlayer *pl11200 = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
+  pl11200.numberOfLoops = -1;
+  soundURL = [[NSBundle mainBundle] URLForResource:@"16000Hz" withExtension:@"mp3"];
+  AVAudioPlayer *pl16000 = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
+  pl16000.numberOfLoops = -1;
+  self.players = @[pl8000, pl10000, pl11200, pl16000];
+  self.currentPlayer = pl8000;
+  self.currentPlayer.volume = 0.5;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +42,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)playingSwitchValueChanged:(id)sender {
+  if (self.playingSwitch.on) {
+    [self.currentPlayer play];
+  } else {
+    [self.currentPlayer pause];
+  }
+}
+
+- (IBAction)toneSelectorValueChanged:(id)sender {
+  NSInteger selectedIndex = self.toneSelector.selectedSegmentIndex;
+  [self.currentPlayer pause];
+  if (selectedIndex >= 0) {
+    AVAudioPlayer *newPlayer = self.players[selectedIndex];
+    self.currentPlayer = newPlayer;
+    newPlayer.volume = self.volumeSlider.value;
+    if (self.playingSwitch.on)
+      [newPlayer play];
+  }
+}
+
+- (IBAction)volumeChanged:(id)sender {
+  double volume = self.volumeSlider.value;
+  NSString *volStr = [NSString stringWithFormat:@"%.4f", volume];
+  self.currentPlayer.volume = volume;
+  self.volumeDisplay.text = volStr;
+}
 @end
